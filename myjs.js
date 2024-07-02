cf9c193ebcad80517d8cee83a7e341d820b880dc
sakuratanaka <167832973+sakuratanaka@users.noreply.github.com>
2024-07-02 10:17:10 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,11 @@
 alert("Hello,Sakura!");
 alert(1+1);
 alert(Math.sqrt(1+1));
-2+3
-'2'+'3'
-Number('2')+Number('3')
-'2'+3
-Number('2')+3
+alert(Math.sqrt(2+3));
+alert(Math.sqrt(1/0));
+alert(Math.sqrt(0/0));
+alert(Math.sqrt(2+3));
+alert(Math.sqrt('2'+'3'));
+alert(Math.sqrt(Number('2')+Number('3')));
+alert(Math.sqrt('2'+3));
+alert(Math.sqrt(Number('2')+3));
